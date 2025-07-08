037097e3479c1bccca8d105542fd5825e4b08352
ob1 <dute000777@gmail.com>
2025-07-08 22:24:50 +0300

    create file 2

--- file.py
+++ file.py
@@ -9,7 +9,7 @@ def add_one(some_list):
         result.append(int(digit))
     return result
 
-assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test123'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
 assert add_one([0]) == [1], 'Test3'
 assert add_one([9]) == [1, 0], 'Test4'
